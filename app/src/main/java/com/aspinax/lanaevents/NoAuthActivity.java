26ce29fd26edefe4ed9828c1bc1aac94df9539dd
Stephen Mwangi <st3v3n.mw@gmail.com>
2020-01-29 15:27:04 +0300

    initial commit

---
+++ app/src/main/java/com/aspinax/lanaevents/NoAuthActivity.java
@@ -0,0 +1,14 @@
+package com.aspinax.lanaevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_auth);
+    }
+}
