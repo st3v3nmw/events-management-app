ce63558001ca3dc7d26058cdbdaaf96c2d30f561
Stephen Mwangi <st3v3n.mw@gmail.com>
2020-07-15 17:40:10 +0300

    added password recovery

--- app/src/main/java/com/aspinax/lanaevents/AgentHomeFragment.java
+++ app/src/main/java/com/aspinax/lanaevents/AgentHomeFragment.java
@@ -5,26 +5,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Map;
-import java.util.Objects;
 
 
 public class AgentHomeFragment extends Fragment {
-    private Database db;
     private Person p;
-    private Event event;
     public AgentHomeFragment() {}
 
     @SuppressLint("SetTextI18n")
@@ -36,11 +29,9 @@ public class AgentHomeFragment extends Fragment {
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
         final FirebaseUser user = mAuth.getCurrentUser();
         final TextView helloFriend = view.findViewById(R.id.hello_friend);
-        final TextView eventNameView = view.findViewById(R.id.event_name);
-        final TextView eventLocationView = view.findViewById(R.id.event_location);
-        final ImageView bannerImage = view.findViewById(R.id.banner_image);
+
         assert user != null;
-        db = new Database(new AsyncResponse() {
+        Database db = new Database(new AsyncResponse() {
             @Override
             public void resultHandler(Map<String, Object> result, int resultCode) {
                 if (resultCode == 0) {
@@ -48,19 +39,6 @@ public class AgentHomeFragment extends Fragment {
                     assert p != null;
                     helloFriend.setText("Hey " + p.getFullName() + ",");
                     p.setAuth(mAuth);
-                } else if (resultCode == 1) {
-                    assert result != null;
-                    for (String eventId: result.keySet()) {
-                        event = (Event) result.get(eventId);
-                        assert event != null;
-                        if (event.start.getSeconds() < Timestamp.now().getSeconds()) {
-                            Log.e("e", event.name);
-                            bannerImage.setImageBitmap(event.imageBitmap);
-                            eventNameView.setText(event.name);
-                            eventLocationView.setText(event.location);
-                            break;
-                        }
-                    }
                 }
             }
 
@@ -71,16 +49,6 @@ public class AgentHomeFragment extends Fragment {
         });
 
         db.read("users", user.getUid(), Person.class, 0);
-        db.compareGreater("events", "end", Timestamp.now(), Event.class, 1);
-
-        MaterialButton attendanceBtn = view.findViewById(R.id.attendanceListBtn);
-        attendanceBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PersonListDialogFragment.newInstance(event).show(Objects.requireNonNull(getActivity())
-                        .getSupportFragmentManager(), "dialog");
-            }
-        });
         return view;
     }
 }
--- app/src/main/java/com/aspinax/lanaevents/ForgotPasswordActivity.java
+++ app/src/main/java/com/aspinax/lanaevents/ForgotPasswordActivity.java
@@ -1,8 +1,20 @@
 package com.aspinax.lanaevents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
@@ -10,5 +22,29 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        MaterialButton recoverBtn = findViewById(R.id.recover_btn);
+        TextInputEditText emailView = findViewById(R.id.email);
+
+        recoverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Objects.requireNonNull(emailView.getText()).toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    emailView.setError(getString(R.string.fname_val));
+                    return;
+                }
+
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Password reset email sent.", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
